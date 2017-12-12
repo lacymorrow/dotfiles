dbb66bfd5688253622b7bce5618e90fcae94b8e7
Lacy Morrow <me@lacymorrow.com>
2017-12-11 16:18:05 -0800

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -115,7 +115,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    "hyperborder"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
