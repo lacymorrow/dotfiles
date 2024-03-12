6493ee5525db2f0d93489d0af0de6c3d2d1d9f62
Lacy Morrow <me@lacymorrow.com>
2024-03-12 15:18:05 -0400

    stuff

---
+++ mac.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+echo "> Clone firmware"
+
+# Clone files
+echo "Cloning the dotfiles"
+git clone --single-branch --branch master https://github.com/lacymorrow/dotfiles.git $dir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Moving any existing dotfiles from ~ to $olddir"
+for file in "$dir"/home/.*; do
+    # mv ~/$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    # ln -s $dir/$file ~/$file
+done
+
