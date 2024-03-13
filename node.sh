be99c6217b11c1b1e64b696025a322d78634eac5
Lacy Morrow <me@lacymorrow.com>
2024-03-13 02:48:08 -0400

    mac

--- node.sh
+++ node.sh
@@ -5,6 +5,14 @@
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+# Source it
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install latest Node
+nvm i node
+
 # Install NPM global packages
 for app in "yarn" \
 	; do
