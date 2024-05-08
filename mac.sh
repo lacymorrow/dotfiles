5f61f6c95c32d1d3b5489f023847d48d92198b31
Lacy Morrow <me@lacymorrow.com>
2024-05-08 10:44:52 -0400

    tweak

--- mac.sh
+++ mac.sh
@@ -12,43 +12,15 @@
 # - Install node packages (depends on node)
 ############################
 
-########## Variables
+# bash "$dir/.osx"
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+# bash "$dir/brew.sh"
 
-##########
+# bash "$dir/node.sh"
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "> Clone firmware"
-
-# Clone files
-# echo "Cloning the dotfiles"
-# git clone --single-branch --branch master https://github.com/lacymorrow/dotfiles.git $dir
-# echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-if [ -d "$dir" ]; then
-
-    # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-    echo "Moving any existing dotfiles from ~ to $olddir"
-
-    # matches all dotfiles, except . and ..
-    for file in "$dir"/home/.[!.]*; do
-        mv  ~/$(basename "$file") $olddir
-        echo "Creating symlink to $file in home directory."
-        ln -s $file ~/$(basename "$file")
-    done
-
-fi
 
 ###############################################################################
 # Copy Files
@@ -63,17 +35,8 @@ fi
 # Manual URL Installs                                                         #
 ###############################################################################
 
-# bash "$dir/.osx"
-
-# bash "$dir/node.sh"
-
-# bash "$dir/brew.sh"
-
-# Install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# for app in "https://symless.com/synergy" \
-# 	"https://cursor.sh" \
-# 	"https://www.privateinternetaccess.com"; do
-# 	open "${app}"
-# done
+for app in "https://symless.com/synergy" \
+	"https://cursor.sh" \
+	"https://www.privateinternetaccess.com"; do
+	open "${app}"
+done
