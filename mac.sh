d9782fbdc9e3628b297596e1b20cc8d05e8efb87
Lacy Morrow <lacymorrow0@gmail.com>
2024-08-01 11:59:59 -0400

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -23,6 +23,8 @@ bash "$dir/brew.sh"
 
 bash "$dir/node.sh"
 
+bash "$dir/symlink_dotfiles.sh"
+
 # Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
